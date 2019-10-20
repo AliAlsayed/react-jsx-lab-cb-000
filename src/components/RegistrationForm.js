9103b559625cde1e2ff524e4e1f3774a9269aba2
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-20 08:23:38 +0000

    Automatically backed up by Learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+class RegistrationForm extends React.Component{
+  render(){
+    return (
+      <form>
+      </form>
+    )
+  }
+}
